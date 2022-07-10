89c19cf4012f566dcfaf81bc65ea9e7a91fe9dff
Hà Văn Được <havanduoc.dev@gmail.com>
2022-07-10 17:16:28 +0700

    TEST - Creating a user

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
+const User = require("../models/Users");
 
-router.get("/", (req, res) => {
-    res.send("this is route auth");
+// REGISTER
+router.get("/register", async (req, res) => {
+    const user = new User({
+        username: "Hà Văn Được",
+        email: "havanduoc.dev@gmail.com",
+        password: "123456",
+    });
+
+    await user.save();
+    res.send("OK");
 });
 
 module.exports = router;
