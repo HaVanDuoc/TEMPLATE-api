0db72f4f13b4a7678cd7ee8663e4fbe5be625807
Hà Văn Được <havanduoc.dev@gmail.com>
2022-07-10 16:45:35 +0700

    middleware and test request

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ mongoose.connect(process.env.MONGODB_URL, () => {
     console.log("Connected to MongoDB");
 });
 
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("welcome to home page");
+});
+app.get("/users", (req, res) => {
+    res.send("welcome to user page");
+});
+
 app.listen(9000, () => {
     console.log("Backend server is running!");
 });
